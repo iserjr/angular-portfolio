4ab1b9595a1975ce789b2e5a6d7b68ef38131dad
Sergio Silva <asteriesc@gmail.com>
2024-04-19 17:10:55 -0300

    feat: formations component

---
+++ src/app/modules/portfolio/components/formations/formations.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-formations',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './formations.component.html',
+  styleUrl: './formations.component.scss'
+})
+export class FormationsComponent {
+
+}
--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 
 import { HeaderComponent } from '../../components/header/header.component';
+import { FormationsComponent } from '../../components/formations/formations.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormationsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
